d52f934e75a73fd9b59b8f1137db592b50ceb20b
Mikee Morales <mikee-mbair@Mikees-Laptop.local>
2024-02-22 09:49:10 -0800

    update carousels

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,10 @@
 import {Container} from "@mui/material";
-import styles from "@/styles/page.module.css";
 import AboutMain from "@/app/about/components/AboutMain";
 
 export default function Portfolio() {
     return (
         <>
-            <Container maxWidth='true' className={styles.main}>
+            <Container maxWidth='true'>
                 <AboutMain/>
             </Container>
         </>
